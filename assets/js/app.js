799a5ece40f782b4603812b84521aa6dcbebc548
theophildio <dtheo.sangma@gmail.com>
2022-02-17 21:54:56 +0600

    Error handling added in Income and Expenses calculation.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,57 +3,48 @@ document.getElementById('calculate').addEventListener('click', function() {
   // Income
   const incomeInput = document.getElementById('income-value');
   const incomeAmount = parseFloat(incomeInput.value);
-
-  // Expenses
-  const foodCost = document.getElementById('food-value');
-  const foodCostAmount = parseFloat(foodCost.value);
-  const rentCost = document.getElementById('rent-value');
-  const rentCostAmount = parseFloat(rentCost.value);
-  const clotheCosts = document.getElementById('clothe-values');
-  const clotheCostsAmount = parseFloat(clotheCosts.value);
-  const calcExpenses = foodCostAmount + rentCostAmount + clotheCostsAmount;
-  console.log(calcExpenses)
-  // Clear expenses fields
-  foodCost.value = '';
-  rentCost.value = '';
-  clotheCosts.value = '';
-
-  // Total Expenses and Balance
-  const totalBalance = incomeAmount - calcExpenses;
-  console.log(totalBalance);
-  const expense = document.getElementById('expenses');
-  const displayExpense = parseFloat(expense.innerText);
-  expense.innerText = calcExpenses;
-  const balance = document.getElementById('balance');
-  const displayBalance = parseFloat(balance.innerText);
-  balance.innerText = totalBalance;
+  if (incomeInput.value == 0) {
+    console.log('opps! income field is empty.');
+  } else if (incomeInput.value > 0) {
+    // Expenses
+    const foodCost = document.getElementById('food-value');
+    const foodCostAmount = parseFloat(foodCost.value);
+    const rentCost = document.getElementById('rent-value');
+    const rentCostAmount = parseFloat(rentCost.value);
+    const clotheCosts = document.getElementById('clothe-values');
+    const clotheCostsAmount = parseFloat(clotheCosts.value);
+    if (foodCost.value == 0 && rentCost.value == 0 && clotheCosts.value == 0) {
+      console.log('opps! expenses fields are empty.')
+    } else if (foodCost.value > 0 && rentCost.value == 0 && clotheCosts.value == 0) {
+      console.log('opps! rent and clothes fields are empty.');
+    } else if (foodCost.value > 0 && rentCost.value > 0 && clotheCosts.value == 0) {
+      console.log('opps! clothes field is empty.');
+    } else if (foodCost.value == 0 && rentCost.value > 0 && clotheCosts.value > 0) {
+      console.log('opps! food field is empty.')
+    } else if (foodCost.value > 0 && rentCost.value == 0 && clotheCosts.value > 0) {
+      console.log('opps! rent field is empty.')
+    } else if (foodCost.value > 0 && rentCost.value > 0 && clotheCosts.value > 0) {
+      const calcExpenses = foodCostAmount + rentCostAmount + clotheCostsAmount;
+      const totalBalance = incomeAmount - calcExpenses;
+      const expense = document.getElementById('expenses');
+      const displayExpense = parseFloat(expense.innerText);
+      if (calcExpenses > incomeAmount) {
+        console.log('opps! over expenses. Reduce your expenses.')
+      } else {
+        expense.innerText = calcExpenses;
+        const balance = document.getElementById('balance');
+        const displayBalance = parseFloat(balance.innerText);
+        balance.innerText = totalBalance;
+      }
+      // Clear expenses fields
+      foodCost.value = '';
+      rentCost.value = '';
+      clotheCosts.value = '';
+    } else {
+      console.log('opps! negative or string value is not allow expense field.')
+    }
+  } else {
+    console.log('opps! negative or string value is not allow in income field.');
+  }
 });
 
-// Saving percentage 
-document.getElementById('saving').addEventListener('click', function() {
-  // Income amount
-  const incomeInput = document.getElementById('income-value');
-  const incomeAmount = parseFloat(incomeInput.value);
-  // Percentage amount
-  const inputPerc = document.getElementById('perc-value');
-  const inputPercValue = inputPerc.value;
-  const getPerc = (incomeAmount / 100) * inputPercValue;
-  // Show saving amount
-  const getSavingAmount =document.getElementById('saving-amount');
-  const displaySavingAmount = parseFloat(getSavingAmount.innerText);
-  const savimgAmount = document.getElementById('saving-amount');
-  const displayTotalSavingAmount = parseFloat(savimgAmount.innerText);
-  savimgAmount.innerText = getPerc;
-  // Clear percentage
-  inputPerc.value = '';
-  // Remaining previous balance
-  const previousBalance = document.getElementById('balance');
-  const displayPreviousBalance = parseFloat(previousBalance.innerText);
-  // Remaining balance
-  const remainingBalance = document.getElementById('remaining-amount');
-  const displayRemainingBalance = parseFloat(remainingBalance.innerText);
-  remainingBalance.innerText = displayPreviousBalance - getPerc;
-
-  // Clear income fields
-  incomeInput.value = '';
-});
---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+// Calculate income and expenses
+document.getElementById('calculate').addEventListener('click', function() {
+  // Income
+  /* const incomeInput = document.getElementById('income-value');
+  const incomeAmount = parseFloat(incomeInput.value);
+  // Expenses
+  const foodCost = document.getElementById('food-value');
+  const foodCostAmount = parseFloat(foodCost.value);
+  const rentCost = document.getElementById('rent-value');
+  const rentCostAmount = parseFloat(rentCost.value);
+  const clotheCosts = document.getElementById('clothe-values');
+  const clotheCostsAmount = parseFloat(clotheCosts.value); */
+  // Total Expenses and Balance
+  /* const calcExpenses = foodCostAmount + rentCostAmount + clotheCostsAmount;
+  const totalBalance = incomeAmount - calcExpenses;
+  const expense = document.getElementById('expenses');
+  const displayExpense = parseFloat(expense.innerText);
+  expense.innerText = calcExpenses;
+  const balance = document.getElementById('balance');
+  const displayBalance = parseFloat(balance.innerText);
+  balance.innerText = totalBalance; */
+    
+  // Clear expenses fields
+  foodCost.value = '';
+  rentCost.value = '';
+  clotheCosts.value = '';
+
+});
+
+// Saving percentage 
+document.getElementById('saving').addEventListener('click', function() {
+  // Income amount
+  const incomeInput = document.getElementById('income-value');
+  const incomeAmount = parseFloat(incomeInput.value);
+  // Percentage amount
+  const inputPerc = document.getElementById('perc-value');
+  const inputPercValue = inputPerc.value;
+  const getPerc = (incomeAmount / 100) * inputPercValue;
+  // Show saving amount
+  const getSavingAmount =document.getElementById('saving-amount');
+  const displaySavingAmount = parseFloat(getSavingAmount.innerText);
+  const savimgAmount = document.getElementById('saving-amount');
+  const displayTotalSavingAmount = parseFloat(savimgAmount.innerText);
+  savimgAmount.innerText = getPerc;
+  // Clear percentage
+  inputPerc.value = '';
+  // Remaining previous balance
+  const previousBalance = document.getElementById('balance');
+  const displayPreviousBalance = parseFloat(previousBalance.innerText);
+  // Remaining balance
+  const remainingBalance = document.getElementById('remaining-amount');
+  const displayRemainingBalance = parseFloat(remainingBalance.innerText);
+  remainingBalance.innerText = displayPreviousBalance - getPerc;
+
+  // Clear income fields
+  incomeInput.value = '';
+});
