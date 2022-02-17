3beee7e2a50464c47193926f33d557722eaa6fbb
theophildio <dtheo.sangma@gmail.com>
2022-02-17 16:38:42 +0600

    Monthly saving calculation added

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,6 @@ document.getElementById('calculate').addEventListener('click', function() {
   // Income
   const incomeInput = document.getElementById('income-value');
   const incomeAmount = parseFloat(incomeInput.value);
-  // Clear income fields
-  incomeInput.value = '';
 
   // Expenses
   const foodCost = document.getElementById('food-value');
@@ -29,4 +27,33 @@ document.getElementById('calculate').addEventListener('click', function() {
   const balance = document.getElementById('balance');
   const displayBalance = parseFloat(balance.innerText);
   balance.innerText = totalBalance;
+});
+
+// Saving percentage 
+document.getElementById('saving').addEventListener('click', function() {
+  // Income amount
+  const incomeInput = document.getElementById('income-value');
+  const incomeAmount = parseFloat(incomeInput.value);
+  // Percentage amount
+  const inputPerc = document.getElementById('perc-value');
+  const inputPercValue = inputPerc.value;
+  const getPerc = (incomeAmount / 100) * inputPercValue;
+  // Show saving amount
+  const getSavingAmount =document.getElementById('saving-amount');
+  const displaySavingAmount = parseFloat(getSavingAmount.innerText);
+  const savimgAmount = document.getElementById('saving-amount');
+  const displayTotalSavingAmount = parseFloat(savimgAmount.innerText);
+  savimgAmount.innerText = getPerc;
+  // Clear percentage
+  inputPerc.value = '';
+  // Remaining previous balance
+  const previousBalance = document.getElementById('balance');
+  const displayPreviousBalance = parseFloat(previousBalance.innerText);
+  // Remaining balance
+  const remainingBalance = document.getElementById('remaining-amount');
+  const displayRemainingBalance = parseFloat(remainingBalance.innerText);
+  remainingBalance.innerText = displayPreviousBalance - getPerc;
+
+  // Clear income fields
+  incomeInput.value = '';
 });
